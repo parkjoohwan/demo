23b2a4b343fccf1d07ed96b476b4e2cda2d8de79
parkjoohwan <jookid94@gmail.com>
2025-10-18 21:28:33 +0900

    temp

--- src/test/java/com/sprint/mission/demo/DemoApplicationTests.java
+++ src/test/java/com/sprint/mission/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.sprint.mission.demo;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class DemoApplicationTests {
 
   @Test
   void contextLoads() {
+  }
+
+  @Test
+  void test(){
+    assertThat("test").isEqualTo("test");
   }
 
 }
